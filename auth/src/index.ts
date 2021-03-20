d7ffa4e28ded00bea320072b69dbf3716d615e65
Swetabh Subham <swetabh.subham@gmail.com>
2021-03-21 04:09:04 +0530

    new

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(json());
 
+
+app.get("/api/users/currentUser",(req,res)=>{
+    res.send("HI there !!")
+})
+
 app.listen(3000,()=>{
-    console.log("Listening to port 3000!")
+    console.log("Listening to port 3000!!!!!")
 })
