c0021f7114606f503136a437d17c63d99eccf6e2
Swetabh Subham <swetabh.subham@gmail.com>
2021-03-27 01:37:23 +0530

    error modeling

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -2,25 +2,33 @@ import {Request , Response , NextFunction} from 'express'
 import {RequestValidationError} from  "../errors/request-validation-error";
 import {DatabaseConnectionError} from "../errors/database-connection-error";
 
+
 export const errorHandler = (err:Error, req:Request, res:Response, next:NextFunction)=>{
      //  fromat arror in to a consistant Error Object with format of
      /*
             error:{
                 message: String[], field?: string
-            }
+            }[]
+            array of objects
       */
     if(err instanceof RequestValidationError){
         const formattedError = err.errors.map(error =>{
-            return {message : error.msg field: error.param };
+            return {message : error.msg ,field: error.param };
         });
         
+        return res.status(500).send({errors : formattedError});
     }
 
     if(err instanceof DatabaseConnectionError){
         console.log("handling this error as  database connection error")
+        return res.status(500).send({errors : [
+            {message : err.reason}
+        ]})
     }
 
     res.status(400).send({
-        message: err.message
+        message: [{
+            message: "Something Went Wrong"
+        }]
     })
 } 
