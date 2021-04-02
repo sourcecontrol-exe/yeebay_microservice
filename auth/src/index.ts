0cf7ab76520a0c3d8bbed8cc63593851ae7c62b0
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-03 03:48:23 +0530

    refractor app.js

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,6 @@
-import express from "express";
 import "express-async-errors";
-import {json} from "body-parser";
-import {currentUserRouter} from './routes/current-user'
-import {signinRouter} from './routes/signin';
-import {signupRouter} from "./routes/signup";
-import {signoutRouter} from "./routes/signout";
-import {errorHandler} from "./middlewares/error-handler"
-import {NotFoundError } from "./errors/not-found-error"
 import mongoose from "mongoose"
-import cookieSession from "cookie-session"
-
-
-const app = express();
-
-app.set('trust proxy', true);
-
-app.use(json());
-
-app.use(cookieSession({
-    signed:false,
-    secure: true
-}))
-
-app.use(signupRouter);
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter); 
-app.use(errorHandler);
-
-app.all('*', async (req,res)=>{
-    throw new NotFoundError;
-})
+import  {app} from "./app";
 
 const start = async () => {
     if(!process.env.JWT_KEY){
