147922bb11838a60f616b8cdc5608cd4a3233bb7
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-04 02:22:39 +0530

    auth global helper function signin

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,8 +1,18 @@
+import  request  from 'supertest';
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose  from 'mongoose';
 import {app} from '../app';
 
 let mongo:any 
+
+declare global {
+    namespace NodeJS {
+        interface Global{
+            signin() : Promise<string[]>;
+        }
+    }
+}
+
 
 beforeAll(async ()=>{
     process.env.JWT_KEY = "asdf";
@@ -27,3 +37,18 @@ afterAll(async () =>{
     await mongo.stop();
     await mongoose.connection.close();
 })
+
+global.signin = async () =>{
+    const email = "test@test.com"
+    const password = "password"
+    const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+        email,password
+    })
+    .expect(200);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+}
