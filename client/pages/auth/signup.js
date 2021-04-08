baec9c3d2670920be63ba03971d5ed82db949616
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-08 10:38:45 +0530

    use-request

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,49 +1,48 @@
-import {useState} from "react";
+import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
-import axios from "axios"
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password
+    },
+    onSuccess: () => Router.push('/')
+  });
 
-export default () =>{
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setError] = useState([]);
+  const onSubmit = async event => {
+    event.preventDefault();
+    
+    await doRequest();
+  };
 
-    const onSubmit =async (event) =>{
-        event.preventDefault();
-        try{
-        const response  = await axios.post("/api/users/signup",{
-             email,password})
-        console.log(response.data)
-        }
-        catch(err){
-            setError(err.response.data.errors);
-        }
-    }
-
-
-    return <form onSubmit={onSubmit}>
-        <h1> Sign Up </h1>
-        <div className = "form-group">
-            <label> Email Address</label>
-            <input 
-            value ={email}
-            onchange = {e => setEmail(e.target.value)}
-            className = 'form-contorol'></input>
-        </div>
-        <div className = "form-group">
-            <label>Password</label>
-            <input value={password}
-            onChange = {e=>setPassword(e.target.value)}
-            className = 'form-contorol' type="password"></input>
-        </div>
-        { errors.length >0 &&(<div className = "alert alert-danger">
-            <h4>Ooops...</h4>
-            <ul className="my-0">
-                {errors.map(err =>(
-                    <li key = {err.message}>{err.message}</li>
-                ))}
-            </ul>
-         </div>
-        )}
-        <button className ="btn btn-primary">Sign Up</button>
-    </form> 
-}
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
+import axios from 'axios';
+
+
 export default ()=>{
     return <h1> landinage</h1>
+
 }
+
+const LandingPage= ()=>{
+    console.log("landimng page");
+    return <h1>Landing Page</h1>;
+}
+
+
+ 
