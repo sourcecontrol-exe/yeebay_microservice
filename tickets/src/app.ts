5a5cad7d21844bc1545d0a182bc91771ee3046b9
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-13 03:48:09 +0530

    ticketing service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import {json} from "body-parser";
-import {currentUserRouter} from './routes/current-user'
-import {signinRouter} from './routes/signin';
-import {signupRouter} from "./routes/signup";
-import {signoutRouter} from "./routes/signout";
+
 import {errorHandler} from "@yeebaytickets/common"
 import {NotFoundError } from "@yeebaytickets/common"
 import cookieSession from "cookie-session"
@@ -20,10 +17,7 @@ app.use(cookieSession({
     secure: process.env.NODE_ENV !== "test"
 }))
 
-app.use(signupRouter);
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter); 
+
 app.use(errorHandler);
 
 app.all('*', async (req,res)=>{
