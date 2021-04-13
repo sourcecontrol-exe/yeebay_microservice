c1ebc56a52bd2acdf0266d3bba6dde0f02b4699e
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-13 22:11:11 +0530

    MONGO_URI

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,8 +6,11 @@ const start = async () => {
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY must be defined");
     }
+    if(!process.env.MONGO_URI){
+        throw new Error("Define MONGO_URI")
+    }
     try{
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser:true,
         useUnifiedTopology:true,
         useCreateIndex:true
