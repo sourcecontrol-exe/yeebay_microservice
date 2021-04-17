8e92ce8c7a042dc885f57f70bd2f0ef310d16490
Swetabh Subham <swetabh.subham@gmail.com>
2021-04-17 21:53:42 +0530

    listener gracefull shutdown

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,16 +1,38 @@
-import nats, {Message} from 'node-nats-streaming';
+import nats, {Message, Stan} from 'node-nats-streaming';
+import {randomBytes} from 'crypto';
+
+
+
 console.clear();
 
-const stan = nats.connect('ticketing', '123',{
+
+
+const stan = nats.connect('ticketing', randomBytes(4).toString("hex"),{
     url:"http://localhost:4222"
 })
 
 stan.on('connect', ()=>{
-    console.log("connected to listener");
+    console.log("connected to listener"); 
 
-    const subscription = stan.subscribe("ticket:created")
+    stan.on('close',()=>{
+        console.log("NATS connection closed!");
+        process.exit();
+    })
+
+    const options = stan.subscriptionOptions().setManualAckMode(true);
+
+    const subscription = stan.subscribe("ticket:created", 'ticking:queue_group', options)
 
     subscription.on('message', (msg:Message) =>{
         console.log("msg recived");
+        const data = msg.getData()
+
+        if(typeof data == 'string'){
+            console.log(`Recived Event #${msg.getSequence()} with data : ${data}`)
+        }
+        msg.ack()
     })
 })
+
+process.on('SIGINT',()=>stan.close());
+process.on("SIGTERM",()=>stan.close());
