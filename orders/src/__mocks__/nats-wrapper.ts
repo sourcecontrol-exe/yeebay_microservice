466bf520c98515d6437d25c14bc4e440d8a18c1e
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-03 04:01:52 +0530

    test files added

---
+++ orders/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,13 @@
+
+export const natsWrapper = {
+  client: {
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      ),
+  },
+};
+  
