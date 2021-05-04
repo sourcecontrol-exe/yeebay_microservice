1114f6e8b7ed0b6de9028f9e3e457a83449abdc1
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-04 21:16:04 +0530

    adding publishers to routes

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,12 +5,14 @@ import {
     NotFoundError
 } from '@yeebaytickets/common'
 import {Order, OrderStatus} from '../models/order' ;
+import {OrderCancelledPublisher} from '../events/publishers/order-cancelled-publisher';
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
 router.delete("/api/orders/:orderId", async (req: Request, res :Response)=>{
     const {orderId} = req.params;
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if(!order){
         throw new NotFoundError();
@@ -22,6 +24,13 @@ router.delete("/api/orders/:orderId", async (req: Request, res :Response)=>{
     order.status = OrderStatus.Cancelled;
     
     await order.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+        id : order.id,
+        ticket:{
+            id: order.ticket.id,
+        }
+    });
 
 
     res.status(204).send(order);
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -10,8 +10,8 @@ import {
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
-
-
+import {OrderCreatedPublisher} from '../events/publishers/order-created-publisher'
+import {natsWrapper} from '../nats-wrapper'
 
 const router = express.Router();
 
@@ -58,6 +58,18 @@ router.post(
     });
     await order.save();
     // Publish an event saying that an order was created
+
+
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id : order.id,
+      status : order.status,
+      userId : order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket:{
+        id: ticket.id,
+        price: ticket.price,
+      }
+    })
 
     res.status(201).send(order);
   }
