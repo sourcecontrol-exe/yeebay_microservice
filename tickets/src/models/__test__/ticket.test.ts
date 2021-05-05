7b71985f094003d02f73ad799056c579ba8ed969
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-05 21:27:23 +0530

    managing versioning

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,30 @@
+import {Ticket} from '../ticket';
+
+it('implements optimistic concurtrnet y test', async () =>{
+    //creat a n instance of a ticket 
+
+    const ticket = Ticket.build({
+        title : 'concert',
+        price : 1,
+        userId: '123'
+    });
+
+
+
+    //save the ticket 
+     ticket.save();    
+    //fetch the ticket twice
+    const firstInstance = await Ticket.findById(ticket.id);
+    //mkae two seperate cahnge to the ticket swe fetched
+    const secondInstance = await Ticket.findById(ticket.id); 
+    //save the first fetched ticket
+
+    firstInstance!.set({price:10});
+    secondInstance!.set({price: 15});
+
+    await firstInstance!.save();
+
+    // save the secons fetched ticket asb expect an error
+
+    await secondInstance!.save();
+})
