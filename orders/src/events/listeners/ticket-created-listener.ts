f5931cffe0355ef61d4f580e9779631721597c40
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-05 19:58:35 +0530

    ticketupdatedListener Added

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,10 +1,20 @@
 import {Message} from 'node-nats-streaming';
 import  { Subjects, Listener, TicketCreatedEvent} from '@yeebaytickets/common';
 import {queueGroupName}  from './queue-group-name';
+import { Ticket } from '../../models/ticket';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
     subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName = queueGroupName;
 
-    onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+    async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+        const {id,title ,price} = data;
+        const ticket = Ticket.build({
+            id,title, price
+        })
+
+        await ticket.save();
+
+        msg.ack();
+    }
 }
---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,26 @@
+import {Message} from "node-nats-streaming";
+import {queueGroupName} from './queue-group-name'
+import {Subjects, Listener, TicketUpdatedEvent} from '@yeebaytickets/common';
+import {Ticket} from '../../models/ticket';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent>{
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+    
+    queueGroupName= queueGroupName;
+
+    async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+        const {id, title ,price} = data;
+        
+        const ticket = await Ticket.findById(id);
+
+        if(!ticket){
+            throw new Error('ticket not found');
+        }
+
+        ticket.set({title, price});
+        
+        await ticket.save();
+
+        msg.ack();
+    }
+}
