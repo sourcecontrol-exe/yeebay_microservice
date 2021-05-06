0d9dc92c029cadd42238145ffb1ab3a82f3ed491
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-06 21:28:31 +0530

    ticket update

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -28,6 +28,7 @@ router.post(
     await ticket.save();
     await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
+      version: ticket.version,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -40,6 +40,7 @@ router.put(
 
     new TicketUpdatedPublisher(natsWrapper.client).publish({
       id : ticket.id,
+      version : ticket.version,
       title : ticket.title,
       price : ticket.price,
       userId : ticket.userId
