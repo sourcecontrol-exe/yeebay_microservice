3334d26fe9dd2547264112740dcdd7ec35796027
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-06 20:47:05 +0530

    listener test setup

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -11,10 +11,7 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent>{
     async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
         const {id, title ,price} = data;
         
-        const ticket = await Ticket.findOne({
-            _id: id,
-            version : data.version-1
-    });
+        const ticket = await Ticket.findByEvent(data);
 
         if(!ticket){
             throw new Error('ticket not found');
