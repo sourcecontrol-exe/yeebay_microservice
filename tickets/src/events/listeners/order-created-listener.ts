760e2321c7e2136be142c8015b80a055b870800c
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-07 20:10:08 +0530

    added publisher to listener

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import {Listener , OrderCreatedEvent, Subjects} from '@yeebaytickets/common';
  import {queueGroupName} from "./queue-group-name";
  import {Message} from 'node-nats-streaming';
  import {Ticket} from "../../models/ticket"
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     subject : Subjects.OrderCreated = Subjects.OrderCreated;
@@ -23,6 +24,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
         ticket.set({ orderId: data.id});
 
         await ticket.save();
+
+       await new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            price : ticket.price,
+            version : ticket.version,
+            title : ticket.title,
+            userId : ticket.userId,
+            orderId : ticket.orderId,
+        });
         
         msg.ack();
     } 
