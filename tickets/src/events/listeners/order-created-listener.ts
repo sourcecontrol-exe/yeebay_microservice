a91e408360ce6243a38e556e52e083e1983e83c2
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-07 18:49:53 +0530

    added orderId attribute to the listener

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,11 +1,29 @@
 import {Listener , OrderCreatedEvent, Subjects} from '@yeebaytickets/common';
  import {queueGroupName} from "./queue-group-name";
  import {Message} from 'node-nats-streaming';
+ import {Ticket} from "../../models/ticket"
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     subject : Subjects.OrderCreated = Subjects.OrderCreated;
     
     queueGroupName = queueGroupName;
 
-    async onMessage(data: OrderCreatedEvent['data'], msg : Message){}
+    async onMessage(data: OrderCreatedEvent['data'], msg : Message){
+        // reach into ticket collection that the order is reserving
+         //if no ticket , throw error
+         // mark the ticke rtas being reserved by  setting its order Id property
+         // save the ticket
+         //ack the message
+        const ticket = await Ticket.findById(data.ticket.id);
+
+        if(!ticket){
+            throw new Error("ticket not found")
+        }
+
+        ticket.set({ orderId: data.id});
+
+        await ticket.save();
+        
+        msg.ack();
+    } 
 }
