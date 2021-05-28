c6f8f797b207b994a11b8d7606c30fd4e66f8c4d
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-29 02:23:36 +0530

    add stripe test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,8 @@ import { OrderStatus } from '@yeebaytickets/common';
 import { app } from '../../app';
 import { Order } from '../../model/order';
 
+jest.mock('../../stripe');
+
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
     .post('/api/payments')
@@ -35,4 +37,44 @@ it('returns a 401 when purchasing an order that doesnt belong to the user', asyn
     .expect(401);
 });
 
-it('returns a 400 when purchasing a cancelled order', async () => {});
+it('returns a 400 when purchasing a cancelled order', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Cancelled,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      orderId: order.id,
+      token: 'asdlkfj',
+    })
+    .expect(400);
+});
+
+it('returns a 204 with valid inputs', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    });
+});
+
