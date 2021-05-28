b36e2e93e642d881b982379a676e356f3af2faae
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-29 02:28:08 +0530

    stripe test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import { OrderStatus } from '@yeebaytickets/common';
 import { app } from '../../app';
 import { Order } from '../../model/order';
-
+import {stripe } from '../../stripe';
 jest.mock('../../stripe');
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
@@ -75,6 +75,12 @@ it('returns a 204 with valid inputs', async () => {
     .send({
       token: 'tok_visa',
       orderId: order.id,
-    });
+    })
+    .expect(201);
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual('tok_visa');
+  expect(chargeOptions.amount).toEqual(20 * 100);
+  expect(chargeOptions.currency).toEqual('usd');
 });
 
