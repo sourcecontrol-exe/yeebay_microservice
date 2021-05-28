cbb2abff8bc0323726bb08c872c3d123488b1bc5
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-29 02:44:05 +0530

    payment model added

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,8 @@ import {
 } from '@yeebaytickets/common';
 import { Order } from '../model/order';
 import {stripe} from "../stripe";
+import {Payment} from '../model/payment';
+
 const router = express.Router();
 
 router.post(
@@ -31,11 +33,17 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency : 'usd',
       amount : order.price*100,
       source: token,
     })
+    const payment = Payment.build({
+      orderId,
+      stripeId : charge.id,
+    })
+
+    await payment.save();
 
     res.status(201).send({ success: true });
   }
