550f8a65cffdf5e297c32460b98149cff62ca3ef
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-29 01:12:28 +0530

    new route

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,29 +1,39 @@
-import express, {
-    Request,
-    Response,
-} from 'express';
-
+import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-
 import {
-    requireAuth,
-    validationRequest,
-    BadRequestError,
-    NotFoundError
-}
-    from '@yeebaytickets/common';
+  requireAuth,
+  validationRequest,
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+} from '@yeebaytickets/common';
 import { Order } from '../model/order';
 
 const router = express.Router();
 
-router.post("/api/payments", requireAuth,
-    [
-        body("token").not().notEmpty(),
-        body('orderId').not().isEmpty()
-    ],
-    async(req: Request, res: Response)=>{
-        res.send({success : true});
+router.post(
+  '/api/payments',
+  requireAuth,
+  [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
+  validationRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
     }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
+    res.send({ success: true });
+  }
 );
 
-export {router  as createChargeRouter}
+export { router as createChargeRouter };
