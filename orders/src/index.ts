e339d05f1a25554e539452fc5bc3c5cc62f3264c
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-29 20:10:15 +0530

    added listener to order

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,6 +4,7 @@ import  {app} from "./app";
 import {TicketCreatedListener} from './events/listeners/ticket-created-listener';
 import {TicketUpdatedListener} from "./events/listeners/ticket-updated-listener";
 import {ExpirationCompleteListener} from './events/listeners/expiration-complete-listener'
+import {PaymentCreatedListener} from "./events/listeners/payment-created-listener";
 import {natsWrapper} from './nats-wrapper';
 
 const start = async () => {
@@ -35,7 +36,7 @@ const start = async () => {
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
     new ExpirationCompleteListener(natsWrapper.client).listen();
-
+    new PaymentCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser:true,
