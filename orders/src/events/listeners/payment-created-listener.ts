64e32edb4c5cba67293460a74ed3720cffe8a739
Swetabh Subham <swetabh.subham@gmail.com>
2021-05-29 20:06:49 +0530

    added listener for order complete;

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,25 @@
+import { Subjects, Listener, PaymentCreatedEvent, OrderStatus} from "@yeebaytickets/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import {queueGroupName} from  './queue-group-name';
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent>{
+    subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+
+    queueGroupName = queueGroupName;
+    async onMessage(data: PaymentCreatedEvent['data'], msg : Message ){
+        const order = await Order.findById(data.orderId);
+
+        if(!order){
+            throw new Error("Order not found");
+        }
+
+        order.set({
+            status: OrderStatus.Complete,
+        });
+        
+        await order.save();
+
+        msg.ack();
+    }
+}
