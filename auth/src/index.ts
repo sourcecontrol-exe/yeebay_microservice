80bcb1de80c9db0153d33b9537177271b1db1af4
Swetabh Subham <swetabh.subham@gmail.com>
2021-07-30 00:59:12 +0530

    new

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 import  {app} from "./app";
 
 const start = async () => {
+    console.log("starting Up")
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY must be defined");
     }
